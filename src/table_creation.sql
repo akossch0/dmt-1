47e641d69f17a35e2b0f7f182fb23b9981bee33c
AndreuCampru <andreucampru@gmail.com>
2025-03-31 19:34:24 +0200

    Last changes.

--- src/table_creation.sql
+++ src/table_creation.sql
@@ -3,28 +3,29 @@
 -- populations table
 CREATE TABLE populations (
     id INTEGER PRIMARY KEY,
-    administrative_unit_id INTEGER,
-    reference_date VARCHAR,
-    district_code VARCHAR,
-    district_name VARCHAR,
-    neighbourhood_code VARCHAR,
-    neighbourhood_name VARCHAR,
-    census_tract_code VARCHAR,
-    number_of_inhabitants INTEGER,
+    administrative_unit_id INTEGER NOT NULL,
+    reference_date VARCHAR NOT NULL,
+    district_code VARCHAR NOT NULL,
+    district_name VARCHAR NOT NULL,
+    neighbourhood_code VARCHAR NOT NULL,
+    neighbourhood_name VARCHAR NOT NULL,
+    census_tract_code VARCHAR NOT NULL,
+    number_of_inhabitants INTEGER NOT NULL,
     FOREIGN KEY (administrative_unit_id) REFERENCES administrative_units(id)
 );
 
 -- administrative_units table
 CREATE TABLE administrative_units (
     id INTEGER PRIMARY KEY,
-    unit_type VARCHAR,
-    district_code VARCHAR,
-    neighbourhood_code VARCHAR,
-    census_tract_code VARCHAR,
-    unit_name VARCHAR,
+    unit_type VARCHAR NOT NULL,
+    district_code VARCHAR NOT NULL,
+    neighbourhood_code VARCHAR NOT NULL,
+    census_tract_code VARCHAR NOT NULL,
+    unit_name VARCHAR NOT NULL,
     perimeter DOUBLE PRECISION,
     area DOUBLE PRECISION,
     scale_range VARCHAR,
-    geometry geometry(Polygon,4326),
+    geometry geometry(Polygon,4326) NOT NULL
 );
+
 
